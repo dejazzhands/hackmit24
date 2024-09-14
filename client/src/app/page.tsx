1ae03e7b53a0237d02e4a8ab940fa4fdde28d09c
samuel e. <49312226+dejazzhands@users.noreply.github.com>
2024-09-14 18:55:38 +0000

    centered div and put in h1 lol

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,11 @@ import Barchart from './components/barchart';
 export default function Home() {
   return (
     <div>
-      <Barchart/>
+      <div className="center-screen">
+        <h1>Beating Hearts</h1>
+      </div>
+      
+      <Barchart />
     </div>
   );
 }
